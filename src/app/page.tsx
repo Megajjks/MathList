f6b3ce6f2dc00a9cd951c10e2ca515ae19a4fb3a
Jayro Salazar <megajjks@gmail.com>
2024-03-21 01:20:28 -0600

    create app and header section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,12 @@ export default function Home() {
       />
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-          content data
+          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            Add colors. Make it yours.
+          </h1>
+          <span className="leading-7 [&:not(:first-child)]:mt-6">
+            Hand-picked themes that you can copy and paste into your apps.
+          </span>
         </div>
       </main>
     </>
